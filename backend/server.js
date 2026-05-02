8f329d8e8c8f4a44abfb4b2cd0edfb9958d11516
Shivam singh <shivamsingh@Shivams-MacBook-Air.local>
2026-05-03 02:32:10 +0530

    fix railway deploy

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,7 @@ require("dotenv").config();
 const app = express();
 
 // --- MIDDLEWARE ---
-app.use(cors({
-    origin: "http://127.0.0.1:5500",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors());
 app.use(express.json());
 
 // --- ROUTES ---
